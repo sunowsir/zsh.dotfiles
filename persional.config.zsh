64dcac04a0f478a600db144b7776e5feee25b951
sun.wang <sun@ASUS-DESKTOP.localdomain>
2026-04-15 00:59:10 +0800

    [upt] update zsh config

---
+++ persional.config.zsh
@@ -0,0 +1,20 @@
+#
+############################################### persional setting
+#
+
+alias ll='ls -al --color=auto'
+alias cls='clear'
+alias ra='ranger'
+
+export EDITOR=vim
+export DEEPSEEK_API_KEY="sk-faba97be76064df3a25336386733c829"
+
+setopt HIST_IGNORE_DUPS
+setopt no_nomatch
+
+figlet "welcome" | lolcat
+
+
+#
+###############################################
+#
